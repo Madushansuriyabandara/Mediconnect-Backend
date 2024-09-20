df2af7bdccdb97426eec663ff0c347742f048455
Tharushika M.T. <thilinitharushika.21@cse.mrt.ac.lk>
2024-09-20 09:45:47 +0530

    view.py

--- api/views.py
+++ api/views.py
@@ -5,44 +5,6 @@ from .models import Note
 
 @api_view(['GET'])
 def getRoutes(request):
-    routes = [
-        {
-            'Endpoint': '/notes/',
-            'method':'GET',
-            'body':None,
-            'description':'Returns an array of notes'
-
-        },
-
-        {
-            'Endpoint': '/notes/id',
-            'method':'GET',
-            'body':None,
-            'description':'Returns a single note object'
-
-        },
-
-        {
-            'Endpoint': '/notes/create/',
-            'method':'POST',
-            'body':{'body':""},
-            'description':'Creates new note with data sent in post request'
-
-        },
-
-        {
-            'Endpoint': '/notes/id/update/',
-            'method':'PUT',
-            'body':{'body':""},
-            'description':'Creates an existing note with data sent in request'
-
-        },
-        {
-            'Endpoint': '/notes/id/delete/',
-            'method':'DELETE',
-            'body': None,
-            'description':'Delete an existing note'
-
-        },
+    routes = [ ###
     ]
     return Response(routes)
