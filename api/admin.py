bc5f3a4266f9b3c8b115f96afd9b318d4b5d6645
Madushansuriyabandara <madushansuriyabandara@gmail.com>
2024-09-23 12:33:27 +0530

    Registered all models in admin.py

--- api/admin.py
+++ api/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
-from .models import Patient, User, Doctor, Appointment, Prescription, Note
+from .models import (
+    Patient, User, Doctor, Appointment, Prescription, Note,
+    Hospital, QUEUEDOCHOS, DocRecord, Session, Medicine,
+    Pharmacy, Report, Schedule, Queue, Feedback, NoteUser
+)
 
 # Register your models here.
 admin.site.register(Patient)
@@ -8,4 +12,14 @@ admin.site.register(Doctor)
 admin.site.register(Appointment)
 admin.site.register(Prescription)
 admin.site.register(Note)
-
+admin.site.register(Hospital)
+admin.site.register(QUEUEDOCHOS)
+admin.site.register(DocRecord)
+admin.site.register(Session)
+admin.site.register(Medicine)
+admin.site.register(Pharmacy)
+admin.site.register(Report)
+admin.site.register(Schedule)
+admin.site.register(Queue)
+admin.site.register(Feedback)
+admin.site.register(NoteUser)
